a3975d77e14726f220da547ebfd1babf660900ae
Christian Muehlhaeuser <muesli@gmail.com>
2026-03-09 23:10:50 +0100

    Settings: bump version to 0x000d

--- src/components/settings/Settings.h
+++ src/components/settings/Settings.h
@@ -369,7 +369,7 @@ namespace Pinetime {
     private:
       Pinetime::Controllers::FS& fs;
 
-      static constexpr uint32_t settingsVersion = 0x000c;
+      static constexpr uint32_t settingsVersion = 0x000d;
 
       struct SettingsData {
         uint32_t version = settingsVersion;
