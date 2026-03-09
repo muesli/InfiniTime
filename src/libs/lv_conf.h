07238bcc1259c25cc996fe7a2bd088741aaa81f5
Christian Muehlhaeuser <muesli@gmail.com>
2026-03-09 22:01:51 +0100

    Add custom Roboto fonts for CleanDigital watch face

--- src/libs/lv_conf.h
+++ src/libs/lv_conf.h
@@ -418,6 +418,8 @@ typedef void* lv_indev_drv_user_data_t;            /*Type of user data in the in
                                LV_FONT_DECLARE(jetbrains_mono_42) \
                                LV_FONT_DECLARE(jetbrains_mono_76) \
                                LV_FONT_DECLARE(open_sans_light) \
+                               LV_FONT_DECLARE(roboto_96) \
+                               LV_FONT_DECLARE(roboto_italic_120) \
                                LV_FONT_DECLARE(fontawesome_weathericons) \
                                LV_FONT_DECLARE(lv_font_sys_48)
 
